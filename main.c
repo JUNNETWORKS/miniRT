6a54630d8c1cd802c972cb0d985f374719548409
Junichi Tanaka <junichi200123@gmail.com>
2021-03-10 02:09:37 +0900

    add: lighting reflection

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 int	initialize_world(t_world *world)
 {
 	world->mlx = mlx_init();
-	world->screen_width = 800;
-	world->screen_height = 800;
+	world->screen_width = 511;
+	world->screen_height = 511;
 	world->win = mlx_new_window(world->mlx,
 		world->screen_width, world->screen_height, "miniRT");
 	world->img.img = mlx_new_image(world->mlx,
@@ -23,13 +23,16 @@ int	raytracing(t_world *world)
 	camera_vec = vec3_init(0, 0, -5);  // スクリーンの少し手前な感じ
 
 	// 球の中心座標
-	t_vec3 square_vec;
-	square_vec = vec3_init(0, 0, 5);  // スクリーンの少し奥な感じ
-	double square_r = 1;  // 半径
+	t_vec3 sphere_vec;
+	sphere_vec = vec3_init(0, 0, 5);  // スクリーンの少し奥な感じ
+	double sphere_r = 1;  // 半径
 
-	for (double y = 0; y < world->screen_height; y++){
-		for (double x = 0; x < world->screen_width; x++){
-			// スクリーン座標からワールド座標への変換
+	// 点光源(light)
+	t_vec3 light_vec;
+	light_vec = vec3_init(-5, 5, -5);
+	for (double x = 0; x < world->screen_width; x++){
+		for (double y = 0; y < world->screen_height; y++){
+			  // スクリーン座標からワールド座標への変換
 			// x,yは[-1,1]へ変換する
 			// スクリーン上の点の三次元空間における位置を計算する
 			t_vec3 screen_vec;
@@ -39,14 +42,49 @@ int	raytracing(t_world *world)
 			t_vec3 dir_vec;
 			dir_vec = vec3_normalize(vec3_sub(screen_vec, camera_vec));
 
+			t_vec3 camera2sphere_vec = vec3_sub(camera_vec, sphere_vec);
+
 			// レイが球に当たったか計算する
 			double a = vec3_mag(dir_vec) * vec3_mag(dir_vec);
-			double b = 2 * vec3_dot(camera_vec, dir_vec);
-			double c = vec3_mag(camera_vec) * vec3_mag(camera_vec) - square_r * square_r;
+			double b = 2 * vec3_dot(camera2sphere_vec, dir_vec);
+			double c = vec3_dot(camera2sphere_vec, camera2sphere_vec) - sphere_r * sphere_r;
 			// 判別式
 			double d = b * b - 4 * a * c;
 			if (d >= 0)
 			{
+				// レイと物体との交点の計算
+				double t = (-b - sqrt(d)) / (2 * a);
+				if (d > 0){  // d > 0 の時, 2つの交点を持つ
+					double t1 = (-b + sqrt(d)) / (2 * a);
+					// 近い方の交点を描画する
+					t = t < t1 ? t : t1;
+				}
+				// レイと物体との交点
+				t_vec3 p_i = vec3_add(camera_vec, vec3_mult(dir_vec, t));
+				if (t < 0)
+					continue;
+
+				// 光の入射ベクトルの計算
+				t_vec3 l;
+				l = vec3_normalize(vec3_sub(light_vec, p_i));
+
+				// 物体面の法線ベクトルの計算
+				t_vec3 n;
+				n = vec3_normalize(vec3_sub(p_i, sphere_vec));
+
+				double ray_deg = vec3_dot(n, l);
+				// 法線ベクトルと入射ベクトルの成す角がπ/2を超えた時, 光は裏側から当たっているので反射は起きない
+				if (ray_deg < 0)
+					ray_deg = 0;
+
+				// 拡散反射光R_dの計算
+				// 光源の強度I_i
+				double I_i = 1;
+				// 拡散反射係数k_d
+				double k_d = 1;
+				// 拡散反射光の放射輝度R_d
+				double R_d = k_d * I_i * ray_deg;
+
 				my_mlx_pixel_put(&world->img, x, y, rgb2hex(255, 0, 0));
 			}
 			else
