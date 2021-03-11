45210cc75afe1e8e7228312ef786dc98bde78889
Junichi Tanaka <junichi200123@gmail.com>
2021-03-12 03:56:25 +0900

    add: show type name in print_world()

--- debug.c
+++ debug.c
@@ -1,5 +1,20 @@
 #include "minirt.h"
 
+char	*get_type_name(enum e_shape type)
+{
+	if (type == PLANE)
+		return ("PLANE");
+	else if (type == SPHERE)
+		return ("SPHERE");
+	else if (type == SQUARE)
+		return ("SQUARE");
+	else if (type == CYLINDER)
+		return ("CYLINDER");
+	else if (type == TRIANGLE)
+		return ("TRIANGLE");
+	return ("INVALID");
+}
+
 void	print_world(t_world *world)
 {
 	printf("world:\t%p\n", world);
@@ -8,7 +23,7 @@ void	print_world(t_world *world)
 	printf("\tobjects:       %p\n", world->objects);
 	t_list *current_lst = world->objects;
 	while (current_lst){
-		printf("\t\ttype: %u\n", ((t_object*)current_lst->content)->type);
+		printf("\t\ttype: %s\n", get_type_name(((t_object*)current_lst->content)->type));
 		current_lst = current_lst->next;
 	}
 }
