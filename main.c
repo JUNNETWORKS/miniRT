0d2ba1c637f0b8654f474d7024b8597e9bd98685
Junichi Tanaka <junichi200123@gmail.com>
2021-03-12 02:36:04 +0900

    move: sphere's calculation functions was moved to sphere.c

--- main.c
+++ main.c
@@ -18,68 +18,33 @@ int		initialize_world(t_world *world)
 
 int		initialize_objects(t_world *world)
 {
-	t_sphere	*sphere;
-	t_plane		*plane;
+	t_object *object;
 
-	if (!(sphere = sphere_init(vec3_init(3, 0, 25), 1)) ||
-		!(ft_lstadd_back_new(&world->objects, sphere)))
+	if (!(object = sphere_init(vec3_init(3, 0, 25), 1)) ||
+		!(ft_lstadd_back_new(&world->objects, object)))
 		return (put_and_return_err("failed malloc object"));
-	if (!(sphere = sphere_init(vec3_init(2, 0, 20), 1)) ||
-		!(ft_lstadd_back_new(&world->objects, sphere)))
+	if (!(object = sphere_init(vec3_init(2, 0, 20), 1)) ||
+		!(ft_lstadd_back_new(&world->objects, object)))
 		return (put_and_return_err("failed malloc object"));
-	if (!(sphere = sphere_init(vec3_init(1, 0, 15), 1)) ||
-		!(ft_lstadd_back_new(&world->objects, sphere)))
+	if (!(object = sphere_init(vec3_init(1, 0, 15), 1)) ||
+		!(ft_lstadd_back_new(&world->objects, object)))
 		return (put_and_return_err("failed malloc object"));
-	if (!(sphere = sphere_init(vec3_init(0, 0, 10), 1)) ||
-		!(ft_lstadd_back_new(&world->objects, sphere)))
+	if (!(object = sphere_init(vec3_init(0, 0, 10), 1)) ||
+		!(ft_lstadd_back_new(&world->objects, object)))
 		return (put_and_return_err("failed malloc object"));
-	if (!(plane = plane_init(vec3_init(0, 0, 0), vec3_init(1,1,1))) ||
-		!(ft_lstadd_back_new(&world->objects, plane)))
+	if (!(object = plane_init(vec3_init(0, 0, 0), vec3_init(1,1,1))) ||
+		!(ft_lstadd_back_new(&world->objects, object)))
 		return (put_and_return_err("failed malloc object"));
 	return (0);
 }
 
-bool	sphere_has_intersection(t_ray ray, t_sphere sphere)
+t_intersection	calc_intersection(t_ray ray, t_object object)
 {
-	t_vec3 camera2sphere_vec = vec3_sub(ray.start, sphere.center);
-	// レイが球に当たったか計算する
-	double a = vec3_mag(ray.direction) * vec3_mag(ray.direction);
-	double b = 2 * vec3_dot(camera2sphere_vec, ray.direction);
-	double c = vec3_dot(camera2sphere_vec, camera2sphere_vec) - sphere.radius * sphere.radius;
-	// 判別式
-	double d = b * b - 4 * a * c;
-	return (d >= 0);
-}
+	if (object.type == PLANE)
+		
+	else if (object.type == SPHERE)
+		return (calc_sphere_intersection(ray, object));
 
-t_intersection calc_sphere_intersection(t_ray ray, t_sphere sphere){
-	t_vec3 camera2sphere_vec = vec3_sub(ray.start, sphere.center);
-	// レイが球に当たったか計算する
-	double a = vec3_mag(ray.direction) * vec3_mag(ray.direction);
-	double b = 2 * vec3_dot(camera2sphere_vec, ray.direction);
-	double c = vec3_dot(camera2sphere_vec, camera2sphere_vec) - sphere.radius * sphere.radius;
-	// 判別式
-	double d = b * b - 4 * a * c;
-
-	// レイと物体との交点の計算
-	double t = (-b - sqrt(d)) / (2 * a);
-	if (d > 0){  // d > 0 の時, 2つの交点を持つ
-		double t1 = (-b + sqrt(d)) / (2 * a);
-		// 近い方の交点を描画する
-		t = t < t1 ? t : t1;
-	}
-
-	// レイと物体との交点
-	t_vec3 p_i = vec3_add(ray.start, vec3_mult(ray.direction, t));
-
-	// 物体面の法線ベクトルの計算
-	t_vec3 n;
-	n = vec3_normalize(vec3_sub(p_i, sphere.center));
-
-	t_intersection intersection;
-	intersection.distance = t;
-	intersection.normal = n;
-	intersection.position = p_i;
-	return intersection;
 }
 
 int	raytracing(t_world *world)
--- sphere.c
+++ sphere.c
@@ -1,6 +1,6 @@
 #include "minirt.h"
 
-t_object *sphere_init(t_vec3 center, double radius)
+t_object		*sphere_init(t_vec3 center, double radius)
 {
 	t_object *sphere;
 	if (!(sphere = malloc(sizeof(t_object))))
@@ -8,5 +8,49 @@ t_object *sphere_init(t_vec3 center, double radius)
 	sphere->type = SPHERE;
 	sphere->center = center;
 	return (sphere);
+}
+
+bool			sphere_has_intersection(t_ray ray, t_object sphere)
+{
+	t_vec3 camera2sphere_vec = vec3_sub(ray.start, sphere.center);
+	// レイが球に当たったか計算する
+	double a = vec3_mag(ray.direction) * vec3_mag(ray.direction);
+	double b = 2 * vec3_dot(camera2sphere_vec, ray.direction);
+	double c = vec3_dot(camera2sphere_vec, camera2sphere_vec) - sphere.radius * sphere.radius;
+	// 判別式
+	double d = b * b - 4 * a * c;
+	return (d >= 0);
+}
+
+t_intersection	calc_sphere_intersection(t_ray ray, t_object sphere)
+{
+	t_vec3 camera2sphere_vec = vec3_sub(ray.start, sphere.center);
+	// レイが球に当たったか計算する
+	double a = vec3_mag(ray.direction) * vec3_mag(ray.direction);
+	double b = 2 * vec3_dot(camera2sphere_vec, ray.direction);
+	double c = vec3_dot(camera2sphere_vec, camera2sphere_vec) - sphere.radius * sphere.radius;
+	// 判別式
+	double d = b * b - 4 * a * c;
+
+	// レイと物体との交点の計算
+	double t = (-b - sqrt(d)) / (2 * a);
+	if (d > 0){  // d > 0 の時, 2つの交点を持つ
+		double t1 = (-b + sqrt(d)) / (2 * a);
+		// 近い方の交点を描画する
+		t = t < t1 ? t : t1;
+	}
+
+	// レイと物体との交点
+	t_vec3 p_i = vec3_add(ray.start, vec3_mult(ray.direction, t));
+
+	// 物体面の法線ベクトルの計算
+	t_vec3 n;
+	n = vec3_normalize(vec3_sub(p_i, sphere.center));
+
+	t_intersection intersection;
+	intersection.distance = t;
+	intersection.normal = n;
+	intersection.position = p_i;
+	return intersection;
 }
 
