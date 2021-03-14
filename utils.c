99e477bd3a4ae545aca054701c5d46b90683f8af
Junichi Tanaka <junichi200123@gmail.com>
2021-03-14 17:51:01 +0900

    WIP: atof()

--- utils.c
+++ utils.c
@@ -41,3 +41,36 @@ size_t		str_c_count(char *str, char c)
 	}
 	return (count);
 }
+
+double		ft_atof(char *str)
+{
+	double	ans;
+	char	**nums;
+
+	if (!str || get_split_size(str, '.') > 2)
+		return 0;
+	nums = ft_split(str, '.');
+	ans = 0;
+	// 整数部分
+	size_t i = 0;
+	while (i < ft_num_len(nums[0]) && ft_isdigit(nums[0][i]))
+	{
+		ans += (nums[0][i] - '0') * pow(10, ft_num_len(nums[0]) - i - 1);
+		i++;
+	}
+	if (ptrarr_len((void**)nums) == 1)
+		return (ans);
+	i = 0;
+	while(i < ft_num_len(nums[1]) && ft_isdigit(nums[1][i]))
+	{
+		ans += (nums[1][i] - '0') / pow(10, i + 1);
+		i++;
+	}
+	free_ptrarr((void**)nums);
+	return (ans);
+}
+
+
+
+
+
